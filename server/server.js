82ab51c580fda12ba6287bca19ca6979cc0431e2
Aleksey <aleksey99@live.com>
2023-02-06 21:37:38 -0500

    starting server code

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const app = express();
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+    res.json({payload: "Sup baud"})
+})
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server listening on ${PORT}`)
+})
