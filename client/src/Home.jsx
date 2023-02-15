272a922adfc6bdfda9b548d5fc59a88ae5138821
Aleksey <aleksey99@live.com>
2023-02-15 12:33:41 -0500

    vital fix

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return ( 
         <div>
-            <h1>This is the home page</h1>
+            <h1>Hello World!</h1>
         </div>
      );
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
   }
 ])
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
