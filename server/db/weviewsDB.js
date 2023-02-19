b0bc2f2a4d1adb3a6cba025c87382e1ca5c9eaa5
alekseycode <aleksey99@live.com>
2023-02-19 12:47:40 -0500

    set up posgreSQL db with knex

--- server/db/weviewsDB.js
+++ server/db/weviewsDB.js
@@ -1,12 +1,18 @@
 const db = require("knex")({
-    client: "mysql",
+    client: "pg",
     connection: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
         port: process.env.DB_PORT,
-    }
-})
+    },
+    pool: {min: 0, max: 10}
+});
+
+
+Promise.resolve(db).then(() => {
+    console.log('Database connected');
+  });
 
 module.exports = db;
