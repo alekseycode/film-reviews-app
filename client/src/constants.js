cc6f78ea45e843bed60ff3c01fedb1bb864bc395
Aleksey <aleksey99@live.com>
2023-02-21 13:55:20 -0500

    set up api call to fetch films from server

---
+++ client/src/constants.js
@@ -0,0 +1,1 @@
+export const API_URL = process.env.WEVIEWS_SERVER_URL;
--- client/src/layouts/FilmsLayout.jsx
+++ client/src/layouts/FilmsLayout.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+// import { API_URL } from "../constants";
+
 export const FilmsLayout = () => {
+    const [filmsData, setFilmsData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/api/films`)
+            .then(async res => setFilmsData(res.data.payload))
+            .catch(e => console.log(e))
+}, []);
+
   return (
       <div className="films">
-        <p>Film1</p>
-        <p>Film2</p>
-        <p>Film3</p>
-        <p>Film4</p>
+          {filmsData && filmsData.map((film, i) => (
+              <div className="film" key={i}>
+                  <p>{film.title}</p>
+            </div>
+        ))}
       </div>
   );
 }
