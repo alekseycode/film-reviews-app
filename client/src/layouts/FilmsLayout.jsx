087912154496d9060ea4f2856a88740b10928af5
Aleksey <aleksey99@live.com>
2023-02-22 14:58:23 -0500

    make each indivual film a link

--- client/src/layouts/FilmsLayout.jsx
+++ client/src/layouts/FilmsLayout.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from 'axios'
 import { API_URL } from "../constants.jsx";
+import {Link} from "react-router-dom"
 
 export const FilmsLayout = () => {
     const [filmsData, setFilmsData] = useState([]);
@@ -14,10 +15,10 @@ export const FilmsLayout = () => {
   return (
       <div className="films">
           {filmsData && filmsData.map((film, i) => (
-              <div className="film" key={i}>
+              <Link to={film.id.toString()} className="film" key={i}>
                   <p>{film.title}</p>
-            </div>
-        ))}
+            </Link>
+          ))}
       </div>
   );
 }
