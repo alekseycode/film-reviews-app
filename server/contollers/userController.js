e75ddfc1acdf152017a231733459ea0eb9d81492
Aleksey <aleksey99@live.com>
2023-02-24 14:51:12 -0500

    add backend route to get users/reviews by film id

--- server/contollers/userController.js
+++ server/contollers/userController.js
@@ -23,4 +23,23 @@ exports.getUsersById = async (req, res) => {
   }
 }
 
+exports.getUsersReviewsByFilmId = async (req, res) => {
+  try {
+
+    const { id } = req.params;
+
+      await db.select('reviews.id', 'reviews.review', 'users.username')
+      .from('reviews')
+        .join('users', 'reviews.user_id', '=', 'users.id')
+        .where('reviews.film_id', id)
+      .then((rows) => {
+        res.json({message: 'Users with reviews', payload: rows})
+      })
+    
+    
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 
