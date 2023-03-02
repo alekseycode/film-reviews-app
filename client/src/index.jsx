fb164639e41b0423e4a867978b75620896189b37
Aleksey <aleksey99@live.com>
2023-03-01 21:18:25 -0500

    add css to log in form

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'login',
-        element: <LoginLayout/>
+        element: <LoginLayout />,
       }
     ]
   }
--- client/src/layouts/LoginLayout.jsx
+++ client/src/layouts/LoginLayout.jsx
@@ -1,7 +1,18 @@
+import '../stylesheets/loginLayout.css'
+
 export const LoginLayout = () => {
   return (  
-      <div>
-          This is the login page
-      </div>
+      <div className="wrapper">
+          <div className="log-in-form">
+              <div className="username-div">
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" id="username"/>
+              </div>
+              <div className="password-div">
+                <label htmlFor="username">Password</label>
+                <input type="text" name="password" id="password" />
+              </div>
+          </div>
+     </div>
   );
 }
