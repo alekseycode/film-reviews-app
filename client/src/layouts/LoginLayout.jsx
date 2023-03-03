af44bb0220a75926b7785d555a836103f5dad83e
Aleksey <aleksey99@live.com>
2023-03-03 13:02:32 -0500

    add register link& log in btn to log in form

--- client/src/layouts/LoginLayout.jsx
+++ client/src/layouts/LoginLayout.jsx
@@ -1,18 +1,24 @@
 import '../stylesheets/loginLayout.css'
+import {Link} from 'react-router-dom'
 
 export const LoginLayout = () => {
   return (  
-      <div className="wrapper">
-          <div className="log-in-form">
-              <div className="username-div">
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username"/>
-              </div>
-              <div className="password-div">
-                <label htmlFor="username">Password</label>
-                <input type="text" name="password" id="password" />
-              </div>
-          </div>
-     </div>
+    <div class="wrapper">
+    <div class="log-in-form">
+      <div class="username-div">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" />
+      </div>
+      <div class="password-div">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </div>
+      <div class="submit-div">
+          <Link to="/register">Create new account</Link>   
+        <button type="submit">Log in</button>
+      </div>
+    </div>
+  </div>
+  
   );
 }
