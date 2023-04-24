79048592754340de668242bade55d6552fdaceeb
Aleksey <aleksey99@live.com>
2023-04-24 15:03:05 -0400

    refactor filmDetails and add loading screen

--- client/src/pages/FilmDetails.jsx
+++ client/src/pages/FilmDetails.jsx
@@ -1,18 +1,37 @@
 import "../stylesheets/filmDetails.css";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { API_URL } from "../constants";
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import LoadingScreen from "../components/LoaderingScreen";
 
 const FilmDetails = () => {
   const { id } = useParams();
-  const [filmPayload, setFilmPayload] = useState(null);
-  const [reviewsPayload, setReviewsPayload] = useState(null);
+  const [film, setFilm] = useState(null);
+  const [reviews, setReviews] = useState([]);
   const [form, setForm] = useState({ review: "" });
+  const [isLoading, setIsLoading] = useState(true);
   const { review } = form;
   const { user } = useContext(AuthContext);
-  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const submissionData = {
+        review,
+        user_id: user.userId,
+        film_id: film.id,
+      };
+
+      await axios.post(`${API_URL}/api/addReview`, submissionData);
+      setForm({ review: "" });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const updateForm = (e) => setForm({ [e.target.name]: e.target.value });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,74 +40,57 @@ const FilmDetails = () => {
           axios.get(`${API_URL}/api/films/${id}`),
           axios.get(`${API_URL}/api/usersReviews/${id}`),
         ]);
-
         if (filmRes.status !== 200 || reviewsRes.status !== 200) {
           throw new Error("Could not load film details and reviews");
         }
-
-        setFilmPayload(filmRes.data);
-        setReviewsPayload(reviewsRes.data);
+        setFilm(filmRes.data.payload[0]);
+        setReviews(reviewsRes.data.payload);
+        setTimeout(() => setIsLoading(false), 200);
       } catch (e) {
         console.log(e);
       }
     };
-
     fetchData();
-  }, [id]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const submissionData = {
-        review,
-        user_id: user.userId,
-        film_id: filmPayload.payload[0].id,
-      };
-
-      await axios.post(`${API_URL}/api/addReview`, submissionData);
-      setForm({ review: "" });
-      navigate(`/films/${filmPayload.payload[0].id}`);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  const updateForm = (e) => setForm({ [e.target.name]: e.target.value });
-
-  if (!filmPayload || !reviewsPayload) {
-    return <div>Loading...</div>;
-  }
+  }, [id, reviews]);
 
   return (
     <div className="film-details">
-      <h1 className="film-title">{filmPayload.payload[0].title}</h1>
-      {reviewsPayload.payload.map((review) => (
-        <div className="review" key={review.id}>
-          <span className="username">{review.username}:</span> ’{review.review}’
-        </div>
-      ))}
-
-      {user.userId ? (
-        <form className="add-review" onSubmit={handleSubmit}>
-          <label className="review-label" htmlFor="review">
-            Add your review
-          </label>
-          <input
-            className="review-input"
-            type="textarea"
-            id="review"
-            name="review"
-            value={review}
-            onChange={updateForm}
-          />
-          <button className="submit-review-btn" type="submit">
-            Add
-          </button>
-        </form>
+      {isLoading ? (
+        <LoadingScreen />
       ) : (
-        <p className="disclaimer">
-          <Link to="/login">Log in</Link> to add a review
-        </p>
+        film && (
+          <>
+            <h1 className="film-title">{film.title}</h1>
+            {reviews.map((review) => (
+              <div className="review" key={review.id}>
+                <span className="username">{review.username}:</span> "
+                {review.review}"
+              </div>
+            ))}
+            {user.userId ? (
+              <form className="add-review" onSubmit={handleSubmit}>
+                <label className="review-label" htmlFor="review">
+                  Add your review
+                </label>
+                <input
+                  className="review-input"
+                  type="textarea"
+                  id="review"
+                  name="review"
+                  value={review}
+                  onChange={updateForm}
+                />
+                <button className="submit-review-btn" type="submit">
+                  Add
+                </button>
+              </form>
+            ) : (
+              <p className="disclaimer">
+                <Link to="/login">Log in</Link> to add a review
+              </p>
+            )}
+          </>
+        )
       )}
     </div>
   );
